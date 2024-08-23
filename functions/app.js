5566ece69a237caf0d5228ed83557a2b9f6cfa1d
Mushfiq Rahaman <mushfiqr@Mushfiqs-MBP.lan>
2024-08-24 02:46:44 +0600

    full update for metlify

---
+++ functions/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const routes = require('../routes');
+const bodyParser = require('body-parser');
+const serverless = require("serverless-http");
+const app = express();
+require('dotenv').config();
+require('../config/db');
+
+
+const PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(`/.netlify/functions/app`, routes); 
+
+module.exports.handler = serverless(app);
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running of PORT: ${PORT}`);
+})
+
+
