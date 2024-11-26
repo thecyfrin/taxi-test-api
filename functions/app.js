d38c1ac4c903e0b4c0145770ad50e2e38a9fd880
Mushfiq Rahaman <mushfiqr@Mushfiqs-MacBook-Pro.local>
2024-11-26 06:47:47 +0600

    what changed

--- functions/app.js
+++ functions/app.js
@@ -5,15 +5,20 @@ const cors = require("cors");
 const http = require("http");
 const adminRoutes = require("../routes/admin-routes");
 const routes = require("../routes");
-const { setupSocket } = require("./socket"); // Import the Socket.IO setup
+const { setupSocket } = require("../config/socket");
 
 const app = express();
 require("dotenv").config();
 require("../config/db");
 
+const apiServer = http.createServer(app); // Server for Express
+const socketServer = http.createServer(); // Separate server for Socket.IO
+const PORT_SOCKET = 3000; // Port for Socket.IO server
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+setupSocket(socketServer);
 
 // Define the routes for admin and root
 app.use("/api/v1/admin/", adminRoutes); // Admin routes (without the Netlify-specific prefix)
@@ -26,25 +31,23 @@ app.use(
 	})
 );
 
-// Create HTTP server
-const server = http.createServer(app);
-
-// Setup Socket.IO
-setupSocket(server);
-
 // Export the handler for serverless
 module.exports.handler = serverless(app);
 
 // Start the local server if not in a serverless environment
 if (process.env.NODE_ENV !== "production") {
 	const PORT = process.env.PORT || 3000;
-	server.listen(PORT, () => {
+	apiServer.listen(PORT, () => {
 		console.log(`Server running on http://localhost:${PORT}`);
 	});
 }
 
-// OLD CODE
+// Start Socket.IO server on port 3000
+socketServer.listen(PORT_SOCKET, () => {
+	console.log(`Socket.IO server is running on http://localhost:${PORT_SOCKET}`);
+});
 
+// OLD CODE
 // const express = require("express");
 // const bodyParser = require("body-parser");
 // const serverless = require("serverless-http");
