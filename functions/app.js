42303c7f7480d0c0727d36232655c84d7cac5a09
Mushfiq Rahaman <81589814+thecyfrin@users.noreply.github.com>
2024-11-26 07:17:47 +0600

    Update app.js

--- functions/app.js
+++ functions/app.js
@@ -13,7 +13,7 @@ require("../config/db");
 
 const apiServer = http.createServer(app); // Server for Express
 const socketServer = http.createServer(); // Separate server for Socket.IO
-const PORT_SOCKET = 3000; // Port for Socket.IO server
+const PORT_SOCKET = 3201; // Port for Socket.IO server
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -46,35 +46,3 @@ if (process.env.NODE_ENV !== "production") {
 socketServer.listen(PORT_SOCKET, () => {
 	console.log(`Socket.IO server is running on http://localhost:${PORT_SOCKET}`);
 });
-
-// OLD CODE
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const serverless = require("serverless-http");
-// const cors = require("cors");
-// const adminRoutes = require("../routes/admin-routes");
-// const routes = require("../routes");
-
-// const app = express();
-// require("dotenv").config();
-// require("../config/db");
-
-// // Body parser middleware
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// // Define the routes for admin and root
-// app.use("/api/v1/admin/", adminRoutes); // Admin routes (without the Netlify-specific prefix)
-// app.use("/api/v1/", routes); // General routes
-
-// // Serve static files
-
-// // Export the handler for serverless
-// module.exports.handler = serverless(app);
-
-// // Local server (useful for local development)
-// app.use(
-// 	cors({
-// 		origin: "https://cochevia.netlify.app/",
-// 	})
-// );
